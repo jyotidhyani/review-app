89616640926db1e67cca6aade695b389ef10de3e
Jyoti Dhyani <jyoti.dhyani@infoedge.com>
2021-03-11 19:55:12 +0530

    More reviews added and buy dont buy buttons

--- src/components/review-item/review-item.jsx
+++ src/components/review-item/review-item.jsx
@@ -9,7 +9,7 @@ const ReviewItem = ({ userName, title, description, buyLink, imageUrl, shouldBuy
       <div className="title">{title}</div>
       <div className="description">{description}</div>
       <div className="buy-link">Product URL: <a href={buyLink}>{buyLink}</a></div>
-      <div className="recommendation">{shouldBuy ? <span><i class="fa fa-check-circle"></i> Yes, I recommend this product</span> : <span><i class="fa fa-times-circle"></i> No, I do not recommend this product</span>}</div>
+      <div className="recommendation">{shouldBuy === "1" ? <span><i class="fa fa-check-circle"></i> Yes, I recommend this product</span> : <span><i class="fa fa-times-circle"></i> No, I do not recommend this product</span>}</div>
       {imageUrl && <div className="product-image">
         <img src={imageUrl} alt="product-image" />
       </div>}
